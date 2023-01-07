adbc470792ca91ecb7e8d80516923e67e27fff84
mienblack <damienfelipe2015@gmail.com>
2023-01-07 19:34:20 -0300

    env

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const PORT = process.env.PORT || 3000
 const express = require("express")
 const path = require('path')
 const apiRoute = require('./routes/api')
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.use("/api", apiRoute)
 app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log("Server running on port", PORT)
 })
